d3c9de3a417a273d71fb29e66956610ebf5a56fd
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-03-07 22:00:49 +0400

    3-deploy_web_static.py - troubleshot

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -55,6 +55,7 @@ def deploy():
     This function created and distributes an archive to your webserver
     """
     path = do_pack()
+    print(path)
     if not path:
         return False
     return do_deploy(path)
