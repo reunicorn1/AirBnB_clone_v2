ef3a60b264cbdb393bdafe88331876613649b27e
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-03-07 18:44:53 +0400

    2-do_deploy_web_static.py - intendation

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,7 +2,7 @@
 """
 2. Deploy archive!
 """
-from fabric.api import run, put
+from fabric.api import run, put, env, cd
 import os
 
 env.hosts = ['100.27.11.128', '54.209.192.180']
@@ -15,7 +15,8 @@ def do_deploy(archive_path):
     put(archive_path, "/tmp")
     filename = archive_path.split('/')[-1].split('.')[0]
     with cd("/tmp"):
-        run("mkdir -p /data/web_static/releases/{} && tar -xzvf {}.tgz - C /data/web_static/releases/{}".format(filename, filename, filename))
+        run("mkdir -p /data/web_static/releases/{} && tar -xzvf {}.tgz -C /data/web_static/releases/{}".format(filename, filename, filename))
+    run('cp -R /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/ && rm -rf /data/web_static/releases/{}/web_static'.format(filename, filename, filename))
     run("rm /tmp/{}.tgz".format(filename))
-    run("ln -s --force /data/web_static/releases/{} /data/web_static/current".format(filename))
+    run("rm /data/web_static/current && ln -s --force /data/web_static/releases/{} /data/web_static/current".format(filename))
     return True
