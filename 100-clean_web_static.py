2f5e3910d67b1dc0996f59ae7b5660705b376ec7
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-03-09 00:37:13 +0400

    100-clean_web_static.py - back to black

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -18,7 +18,7 @@ def do_clean(number=0):
         for result in results:
             local("rm {}".format(result))
     with cd('/data/web_static/releases'):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for i in range(number)]
-        [run("rm -rf./{}".format(a)) for a in archives]
+        results = run("ls -tr web_static_* -d").split()
+        results = results[:-number]
+        for result in results:
+            run("rm -rf {}".format(result))
