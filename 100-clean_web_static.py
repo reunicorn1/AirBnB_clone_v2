b0433b6b1ea81ea517203db34c930f4154a68469
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-03-08 21:45:45 +0400

    100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -2,7 +2,7 @@
 """
 4. Keep it clean!
 """
-from fabric.api import run. local, cd, lcd
+from fabric.api import run, local, cd, lcd, env
 
 env.hosts = ['100.27.11.128', '54.209.192.180']
 
@@ -13,13 +13,16 @@ def do_clean(number=0):
     """
     if number == 0:
         number = 1
-    with lcd('/versions'):
-        results = local("ls web_static* | head -n -{}".format(number)).split('\n')
+    with lcd('./versions'):
+        results = local("ls web_static*")
+    results = results.stdout.strip().split('\n').sort()[:-(int(number))]
+
+"""
         for result in results:
             local("rm -rf {}".format(result))
+
     with cd('/data/web_static/releases'):
         results = run("ls web_static* -d | head -n -{}".format(number)).split('\n')
         for result in results:
             run("rm -rf {}".format(result))
-
-
+"""
