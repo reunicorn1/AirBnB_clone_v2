fd495822f04e5d46c75bf554dd5e2d61fa2412d3
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-03-11 01:07:51 +0400

    100-clean_web_static.py - origins

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,30 +1,25 @@
 #!/usr/bin/python3
-# Fabfile to delete out-of-date archives.
-import os
-from fabric.api import *
+"""
+4. Keep it clean!
+"""
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+from fabric.api import run, local, cd, lcd, env
+
+env.hosts = ['100.27.11.128', '54.209.192.180']
 
 
 def do_clean(number=0):
-    """Delete out-of-date archives.
-
-    Args:
-        number (int): The number of archives to keep.
-
-    If number is 0 or 1, keeps only the most recent archive. If
-    number is 2, keeps the most and second-most recent archives,
-    etc.
+    """
+    This function deletes out-of-date archives
     """
     number = 1 if int(number) == 0 else int(number)
-
-    archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(number)]
-    with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
-
-    with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+    with lcd('./versions'):
+        results = local("ls web_static* | sort", capture=True)
+        results = sorted(results.stdout.split('\n'))[:-number]
+        for result in results:
+            local("rm {}".format(result))
+    with cd('/data/web_static/releases'):
+        results = run("ls -tr web_static_* -d").split()
+        results = results[:-number]
+        for result in results:
+            run("rm -rf {}".format(result))
