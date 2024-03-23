d2e0de9599ebb3e2dfa4f66e9e30d5f1484053aa
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-03-23 15:04:14 +0400

    0-hello_route.py - better documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,11 +8,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     """
     This function returns Hello HBNB page
     """
     return 'Hello HBNB!'
+
 
 app.run(host='0.0.0.0', port=5000)
