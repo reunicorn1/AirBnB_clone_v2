f96b68ffab1beffd2c5b6e6894b421fbe9085281
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-03-23 15:15:26 +0400

    0-setup_web_static.sh

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """
 0. Hello Flask!
+Starts a Flask web application
+
+Application is listening on 0.0.0.0, port 5000
 """
 
 from flask import Flask
-
 
 app = Flask(__name__)
 
@@ -17,4 +19,5 @@ def hello():
     return 'Hello HBNB!'
 
 
-app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
