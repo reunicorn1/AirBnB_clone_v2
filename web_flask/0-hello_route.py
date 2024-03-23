a0a898fe047b167c89809558b59d98c8c5115cb0
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-03-23 14:56:08 +0400

    0-hello_route.py - running on 0.0.0.0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,11 +8,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello():
-    return (“Hello HBNB!”)
+    return 'Hello HBNB!'
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+app.run(host='0.0.0.0', port=5000)
