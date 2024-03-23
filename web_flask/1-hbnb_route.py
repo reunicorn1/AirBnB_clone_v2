8312366de6f30369251c620213a0a26d748d600f
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-03-23 15:26:42 +0400

    1-hbnb_route.py

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This script starts a web application listening on
+0.0.0.0, port 5000
+
+with two routes:
+    /: display “Hello HBNB!”
+    /hbnb: display “HBNB”
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    This function dispays a web page of "Hello HBNB!”
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    This function displays "HBNB" web page
+    """
+    return 'HBNB'
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
