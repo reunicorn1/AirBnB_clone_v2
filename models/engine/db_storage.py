a253530c284fe1fb32790b89054f07758a0723fa
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-03-24 16:31:22 +0400

    correction of dbstorage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -32,7 +32,7 @@ class DBStorage:
         db = getenv("HBNB_MYSQL_DB")
 
         self.__engine = create_engine(
-            "mysql+mysqldb://{}:{}@127.0.0.1/{}".format(user, pwd, host, db),
+            "mysql+mysqldb://{}:{}@{}/{}".format(user, pwd, host, db),
             pool_pre_ping=True,
         )
 
